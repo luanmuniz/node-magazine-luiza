68cbd1029135255eeec75fe90031f5015b475995
Fernando Daciuk <f.daciuk@gmail.com>
2016-01-11 11:52:08 -0200

    Improve tests

--- test/unit/catalog-test.js
+++ test/unit/catalog-test.js
@@ -6,23 +6,24 @@ const parser = require('xml2js').parseString;
 const MagazineLuizaAPI = require('../../index');
 const catalogResult = require('../mock/json/catalog-result.json');
 
-describe('# MAGAZINE LUIZA API - CATALOG', () => {
-	var magazineLuiza;
-	before(() => {
+describe('# MAGAZINE LUIZA API - CATALOG', function() {
+	let magazineLuiza;
+	before(function() {
 		magazineLuiza = new MagazineLuizaAPI('0000');
 	});
 
-	it('Should MagazineLuizaAPI has ownProperty catalog', () => {
+	it('Should MagazineLuizaAPI has ownProperty catalog', function() {
 		expect(magazineLuiza).has.ownProperty('catalog');
 	});
 
-	it('Should catalog to be an object', () => {
+	it('Should catalog to be an object', function() {
 		expect(magazineLuiza.catalog).to.be.an('object');
 	});
 
-	it('Should parse XML to JSON', () => {
+	it('Should parse XML to JSON', function() {
 		return magazineLuiza.catalog.getProducts()
 			.then((data) => {
+				console.log(data);
 				return expect(data).to.be.deep.equal(catalogResult);
 			})
 		;
