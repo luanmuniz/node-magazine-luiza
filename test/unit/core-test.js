2fbac8de507d2776a40e5f9b11b92d69130f5630
Fernando Daciuk <f.daciuk@gmail.com>
2016-01-11 19:27:09 -0200

    Add more tests to core

--- test/unit/core-test.js
+++ test/unit/core-test.js
@@ -3,6 +3,7 @@
 require('../mock/got-mock');
 const expect = require('chai').expect;
 const core = require('../../lib/core');
+const MagazineLuizaAPI = require('../../index');
 
 describe('# MAGAZINE LUIZA API - CORE', function() {
 	it('Should core API has a prodUrl property', function() {
@@ -18,5 +19,23 @@ describe('# MAGAZINE LUIZA API - CORE', function() {
 		return core.makeRequest('get', 'wrongPath')
 			.catch(err => expect(err).to.be.equal('Request Error'))
 		;
+	});
+
+	it('Should return error when file type is different from XML',
+	function() {
+		return core.requestSuccess({})
+			.catch(err => expect(err).to.be.an('error'))
+		;
+	});
+
+	it('Should return an object even new is not used', function() {
+		const magazineLuiza = MagazineLuizaAPI('0000');
+		expect(magazineLuiza).to.be.instanceof(MagazineLuizaAPI);
+	});
+
+	it('Should use development URL when pass dev environment variable',
+	function() {
+		core.init('0000', 'development');
+		expect(core.mainUrl).to.be.equal('http://b2b-xml.sandbox.luizalabs.com');
 	});
 });
