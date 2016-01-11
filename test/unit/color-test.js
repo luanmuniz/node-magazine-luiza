a3faed662f62e7c6a0255e9f1a05e40128f70883
Fernando Daciuk <f.daciuk@gmail.com>
2016-01-11 15:22:12 -0200

    Fix color test

--- test/unit/color-test.js
+++ test/unit/color-test.js
@@ -6,13 +6,21 @@ const parser = require('xml2js').parseString;
 const MagazineLuizaAPI = require('../../index');
 const catalogResult = require('../mock/json/catalog-result.json');
 
-describe('# MAGAZINE LUIZA API - COLORS', function() {
+describe('# MAGAZINE LUIZA API - CATALOG - COLORS', function() {
 	let magazineLuiza;
 	before(function() {
 		magazineLuiza = new MagazineLuizaAPI('0000');
 	});
 
-	it('Should magazineLuiza has ownProperty colors', function() {
-		expect(magazineLuiza).has.ownProperty('colors');
+	it('Should catalog has a method getColors()', function() {
+		expect(magazineLuiza.catalog).has.property('getColors');
+	});
+
+	it('Should getColors() return an array of objects with colors', function() {
+		return magazineLuiza.catalog.getColors()
+			.then(data => {
+				console.log(data);
+			})
+		;
 	});
 });
