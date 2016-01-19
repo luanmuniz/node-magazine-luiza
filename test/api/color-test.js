d0727ff123b41c6af16348fe60c60f2194a258da
Fernando Daciuk <f.daciuk@gmail.com>
2016-01-19 08:56:01 -0200

    Add test to get products

--- test/api/color-test.js
+++ test/api/color-test.js
@@ -4,10 +4,10 @@ const expect = require('chai').expect;
 const MagazineLuizaAPI = require('../../index');
 const PARTNER_ID = process.env.PARTNER_ID;
 
-describe('# [API] MAGAZINE LUIZA - COLORS', function() {
-	let magazineLuiza = new MagazineLuizaAPI(PARTNER_ID, 'production');
+describe('# [API] MAGAZINE LUIZA - CATALOG - COLORS', function() {
+	const magazineLuiza = new MagazineLuizaAPI(PARTNER_ID);
 
-	it('Should return all products', function() {
+	it('Should return all product colors', function() {
 		return magazineLuiza.catalog.getColors()
 			.then(colors => expect(colors).to.be.an('array'))
 		;
