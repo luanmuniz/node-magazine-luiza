a2db988499b055d3617b14efead1ef333252aeca
Fernando Daciuk <f.daciuk@gmail.com>
2016-01-19 14:41:54 -0200

    Add timeout to solve test for get products catalog

--- test/api/products-test.js
+++ test/api/products-test.js
@@ -4,16 +4,14 @@ const expect = require('chai').expect;
 const MagazineLuizaAPI = require('../../index');
 const PARTNER_ID = process.env.PARTNER_ID;
 
-describe.skip('# [API] MAGAZINE LUIZA - CATALOG - PRODUCTS', function() {
+describe('# [API] MAGAZINE LUIZA - CATALOG - PRODUCTS', function() {
+	this.timeout(90000);
 	const magazineLuiza = new MagazineLuizaAPI(PARTNER_ID);
 
 	it('Should return all products', function() {
 		return magazineLuiza.catalog.getProducts()
 			.then(data => {
-				console.log(data);
-			})
-			.catch(err => {
-				console.log(err);
+				return expect(data).to.be.an('array');
 			})
 		;
 	});
