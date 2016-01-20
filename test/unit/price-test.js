91ca1e32bb0c70492f7bddde6b0ea763dbea10fb
Fernando Daciuk <f.daciuk@gmail.com>
2016-01-20 20:01:43 -0200

    Add test to get price

--- test/unit/price-test.js
+++ test/unit/price-test.js
@@ -2,6 +2,7 @@
 
 const expect = require('chai').expect;
 const MagazineLuizaAPI = require('../../index');
+const productPriceResult = require('../mock/json/product-price-result.json');
 
 describe('# MAGAZINE LUIZA API - CATALOG - PRICE', function() {
 	let magazineLuiza;
@@ -19,6 +20,17 @@ describe('# MAGAZINE LUIZA API - CATALOG - PRICE', function() {
 				return expect(err.message).to.be.equal(
 					'You must pass product ID and Model'
 				)
+			})
+		;
+	});
+
+	it('Should getPrice() return an object with price of product', function() {
+		const productId = '0000000';
+		const productModel = '00';
+
+		return magazineLuiza.catalog.getPrice(productId, productModel)
+			.then(productPrice => {
+				expect(productPrice).to.be.deep.equal(productPriceResult);
 			})
 		;
 	});
