303404e2b966c5f1abf165ad1a6a003ca9be0f67
Fernando Daciuk <f.daciuk@gmail.com>
2016-01-20 15:04:14 -0200

    Add test to verify if id and model were passed by getPrice method

--- test/unit/price-test.js
+++ test/unit/price-test.js
@@ -12,4 +12,14 @@ describe('# MAGAZINE LUIZA API - CATALOG - PRICE', function() {
 	it('Should catalog has a method getPrice()', function() {
 		expect(magazineLuiza.catalog).to.have.a.property('getPrice');
 	});
+
+	it('Should return an error if dont pass id or model', function() {
+		return magazineLuiza.catalog.getPrice()
+			.catch(err => {
+				return expect(err.message).to.be.equal(
+					'You must pass product ID and Model'
+				)
+			})
+		;
+	});
 });
