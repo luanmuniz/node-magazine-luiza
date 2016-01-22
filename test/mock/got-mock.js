f3d4ff2f3d714ee71b9b7abe860c9855cf687544
Fernando Daciuk <f.daciuk@gmail.com>
2016-01-22 21:52:16 -0200

    Add mock to checkout on got-mock

--- test/mock/got-mock.js
+++ test/mock/got-mock.js
@@ -20,10 +20,11 @@ let xmls = {
 		xmlMockPath.concat('price.xml')
 	),
 	ConsultaCEP: path.resolve.apply(path, xmlMockPath.concat('cep.xml')),
-	CalculaFrete: path.resolve.apply(path, xmlMockPath.concat('shipping.xml'))
+	CalculaFrete: path.resolve.apply(path, xmlMockPath.concat('shipping.xml')),
+	FinalizaPedido: path.resolve.apply(path, xmlMockPath.concat('checkout.xml'))
 };
 
-got.get = (url) => {
+got.get = (url, options) => {
 	let chosenXml = url.split(/\/(\w+).asp/gi)[1];
 	if(!chosenXml) {
 		return Promise.reject(new Error('Wrong URL'));
@@ -34,15 +35,11 @@ got.get = (url) => {
 
 got.post = (url, options) => {
 	let chosenXml = options.headers.SOAPAction.split(/org\/(\w+)/gi)[1];
-
 	if(!chosenXml) {
 		return Promise.reject(new Error('Error post. Pass a SOAP Action'));
 	}
-
-	let xml = {
-		body: fs.readFileSync(xmls[chosenXml], 'utf8')
-	};
-	return Promise.resolve(xml);
+	let xml = fs.readFileSync(xmls[chosenXml], 'utf8');
+	return Promise.resolve({ body: xml });
 };
 
 module.exports = got;
