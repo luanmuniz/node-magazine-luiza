e84f7956c99b5795a7dd4b951133d2d2d4419de0
Fernando Daciuk <f.daciuk@gmail.com>
2016-01-24 09:35:38 -0200

    Add test to updateOrder

---
+++ test/unit/update-order-test.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const expect = require('chai').expect;
+const MagazineLuizaAPI = require('../../index');
+const magazineLuiza = new MagazineLuizaAPI('0000');
+
+describe('# MAGAZINE LUIZA API - ORDER - UPDATE ORDER', function() {
+	it('Should order has ownProperty updateOrder', function() {
+		expect(magazineLuiza.order).to.have.a.property('updateOrder');
+	});
+});
