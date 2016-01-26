4d9ef4635eeb63c5aee0fef6c765800efba71487
Fernando Daciuk <f.daciuk@gmail.com>
2016-01-26 20:03:54 -0200

    Add tests for post-sale

---
+++ test/unit/post-sale-test.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const expect = require('chai').expect;
+const MagazineLuizaAPI = require('../../index');
+const magazineLuiza = new MagazineLuizaAPI('0000');
+
+describe('# MAGAZINE LUIZA API - TRACKING', function() {
+	it('Should MagazineLuizaAPI has ownProperty postSale', function() {
+		expect(magazineLuiza).has.ownProperty('postSale');
+	});
+
+	it('Should postSale to be an object', function() {
+		expect(magazineLuiza.postSale).to.be.an('object');
+	});
+});
