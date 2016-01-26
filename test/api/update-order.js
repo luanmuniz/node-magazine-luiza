dffee1e887c8a4abb6355b118f271bc14df0f803
Fernando Daciuk <f.daciuk@gmail.com>
2016-01-26 17:43:33 -0200

    Add integration tests for update order

---
+++ test/api/update-order.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const expect = require('chai').expect;
+const checkout = require('../helpers/checkout');
+const MagazineLuizaAPI = require('../../index');
+const PARTNER_ID = process.env.PARTNER_ID;
+const magazineLuiza = new MagazineLuizaAPI(PARTNER_ID, 'development');
+
+describe('# [API] MAGAZINE LUIZA - ORDER - UPDATE', function() {
+	this.timeout(10000);
+
+	it('Should approve an order', function() {
+		const order = magazineLuiza.order;
+		return checkout(magazineLuiza)
+			.then(response => {
+				console.log(response);
+				return order.update({
+					orderNumber: response.orderNumber,
+					cpf: response.cpf,
+					approved: true
+				});
+			})
+			.then(response => {
+				expect(response).to.be.an('object');
+			})
+		;
+	});
+});
