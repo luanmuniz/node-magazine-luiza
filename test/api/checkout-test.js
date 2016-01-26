d40c764a61d15584aad5d5bd209bd8a7d4fc6bde
Fernando Daciuk <f.daciuk@gmail.com>
2016-01-26 16:46:00 -0200

    Improve checkout integration test

--- test/api/checkout-test.js
+++ test/api/checkout-test.js
@@ -1,31 +1,41 @@
 'use strict';
 
 const expect = require('chai').expect;
-const getProducts = require('../helpers/get-products');
+const calculateShipping = require('../helpers/calculate-shipping');
 const MagazineLuizaAPI = require('../../index');
 const PARTNER_ID = process.env.PARTNER_ID || '0000';
-const magazineLuiza = new MagazineLuizaAPI(PARTNER_ID);
+const magazineLuiza = new MagazineLuizaAPI(PARTNER_ID, 'development');
 
-describe.skip('# [API] MAGAZINE LUIZA - ORDER - CHECKOUT', function() {
+describe('# [API] MAGAZINE LUIZA - ORDER - CHECKOUT', function() {
 	it('Should return a product', function() {
-		let sessionKey = '0';
+		this.timeout(15000);
 
-		return getProducts(magazineLuiza)
-			.then(products => {
-				return magazineLuiza.order.calculateShipping({
-					sessionKey: sessionKey,
-					cpf: '04026452000',
-					cep: '12312123',
-					products: [
-						Object.assign({}, products[9], { quantity: 1 }),
-						Object.assign({}, products[8], { quantity: 1 })
-					]
-				});
-			})
+		const order = magazineLuiza.order;
+		return calculateShipping(magazineLuiza)
 			.then(cart => {
-				expect(cart).to.be.an('object');
+				return order.checkout(Object.assign({}, cart, {
+					address: {
+						street: 'Rua Miguel Mentem',
+						number: '100',
+						complement: 'Sala 2',
+						district: 'Vila Guilherme',
+						city: 'São Paulo',
+						state: 'SP',
+						referency: 'Próximo à algum lugar',
+					},
+					phone: {
+						ddd: '11',
+						number: '999991234'
+					},
+					name: 'Maria Antonieta Silva',
+					email: 'email@premiado.com',
+					stateRegistration: 'ISENTO',
+					partnerOrderNumber: Math.ceil(Math.random() * 9999)
+				}))
 			})
-			.catch(err => console.log(err))
+			.then(response => {
+				return expect(response).to.be.an('object');
+			})
 		;
 	});
 });
