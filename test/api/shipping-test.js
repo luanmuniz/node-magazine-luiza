42f5cc24b4b6bf24bdcd0973e3e3c0c8d624c1e6
Fernando Daciuk <f.daciuk@gmail.com>
2016-01-26 15:45:25 -0200

    Improve calculate shipping api tests

--- test/api/shipping-test.js
+++ test/api/shipping-test.js
@@ -1,30 +1,35 @@
 'use strict';
 
 const expect = require('chai').expect;
+const getProducts = require('../helpers/get-products');
 const MagazineLuizaAPI = require('../../index');
 const PARTNER_ID = process.env.PARTNER_ID;
 const magazineLuiza = new MagazineLuizaAPI(PARTNER_ID, 'development');
 
 describe('# [API] MAGAZINE LUIZA - ORDER - CALCULATE SHIPPING', function() {
+	 this.timeout(5000);
+
 	it('Should calculate shipping', function() {
-			const options = {
-			sessionKey: '0',
-			cpf: '911.850.433-73',
-			cep: '17120-000',
-			products: [{
-				id: '2083101',
-				model: '00',
-				quantity: 1
-			},{
-				id: '0235175',
-				model: '00',
-				quantity: 1
-			}]
-		};
-		return magazineLuiza.order.calculateShipping(options)
-			.then(data => {
-				expect(data).to.be.an('object');
+		return getProducts(magazineLuiza)
+			.then(products => {
+				const cart = {
+					sessionKey: 0,
+					cpf: '911.850.433.73',
+					cep: '17120-000',
+					products: [{
+						id: products[0].id,
+						model: products[0].model,
+						quantity: 2
+					},{
+						id: products[1].id,
+						model: products[1].model,
+						quantity: 3
+					}]
+				};
+
+				return magazineLuiza.order.calculateShipping(cart);
 			})
+			.then(data => expect(data).to.be.an('object'))
 		;
 	});
 });
