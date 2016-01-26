2b9e1a9c3393e1cb76502f99ceff120e63db4dc9
Fernando Daciuk <f.daciuk@gmail.com>
2016-01-26 17:43:17 -0200

    Fix checkout api tests

--- test/api/checkout-test.js
+++ test/api/checkout-test.js
@@ -3,7 +3,7 @@
 const expect = require('chai').expect;
 const checkout = require('../helpers/checkout');
 const MagazineLuizaAPI = require('../../index');
-const PARTNER_ID = process.env.PARTNER_ID || '0000';
+const PARTNER_ID = process.env.PARTNER_ID;
 const magazineLuiza = new MagazineLuizaAPI(PARTNER_ID, 'development');
 
 describe('# [API] MAGAZINE LUIZA - ORDER - CHECKOUT', function() {
