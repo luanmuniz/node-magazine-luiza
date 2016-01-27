6b85d854f6b2cad5ee61c22cc01b1676a8313247
Fernando Daciuk <f.daciuk@gmail.com>
2016-01-27 19:52:14 -0200

    Add catch method for get error on checkout api test and improve tests

--- test/api/checkout-test.js
+++ test/api/checkout-test.js
@@ -2,6 +2,7 @@
 
 const expect = require('chai').expect;
 const checkout = require('../helpers/checkout');
+const checkoutResult = require('../mock/json/checkout-result.json');
 const MagazineLuizaAPI = require('../../index');
 const PARTNER_ID = process.env.PARTNER_ID;
 const magazineLuiza = new MagazineLuizaAPI(PARTNER_ID, 'development');
@@ -12,7 +13,12 @@ describe('# [API] MAGAZINE LUIZA - ORDER - CHECKOUT', function() {
 
 		return checkout(magazineLuiza)
 			.then(response => {
-				return expect(response).to.be.an('object');
+				return expect(response).to.have.all.keys(
+					Object.keys(checkoutResult)
+				);
+			})
+			.catch(err => {
+				expect(err.err).to.contains.all.keys('status', 'message');
 			})
 		;
 	});
