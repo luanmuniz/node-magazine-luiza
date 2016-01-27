ed07d92b88ff3cc9676b5a8e91828620f756cabc
Fernando Daciuk <f.daciuk@gmail.com>
2016-01-27 18:10:41 -0200

    Add tests for API Error on tracking

--- test/unit/tracking-test.js
+++ test/unit/tracking-test.js
@@ -4,6 +4,7 @@ require('../mock/got-mock');
 const expect = require('chai').expect;
 const MagazineLuizaAPI = require('../../index');
 const trackingResult = require('../mock/json/tracking-result.json');
+const trackingHelper = require('../../lib/post-sale/tracking-helper');
 
 describe('# MAGAZINE LUIZA API - POST SALE - TRACKING', function() {
 	let magazineLuiza;
@@ -55,5 +56,19 @@ describe('# MAGAZINE LUIZA API - POST SALE - TRACKING', function() {
 				expect(track).to.be.deep.equal(trackingResult);
 			})
 		;
-	})
+	});
+
+	it('Should return an error if returned status is different from zero',
+	function() {
+		const options = {
+			idStatus: '1',
+			Mensagem: 'Some error message'
+		};
+
+		return trackingHelper.parseObject(options)
+			.catch(err => {
+				expect(err.err).to.have.all.keys('status', 'message');
+			})
+		;
+	});
 });
