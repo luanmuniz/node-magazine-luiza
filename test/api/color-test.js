ba1cdd9f4a4943bf8eb25eb164f77105e6810a19
Fernando Daciuk <f.daciuk@gmail.com>
2016-02-03 19:31:49 -0200

    Improve color api test

--- test/api/color-test.js
+++ test/api/color-test.js
@@ -6,10 +6,21 @@ const PARTNER_ID = process.env.PARTNER_ID;
 const magazineLuiza = new MagazineLuizaAPI(PARTNER_ID);
 
 describe('# [API] MAGAZINE LUIZA - CATALOG - COLORS', function() {
+	let result;
+	before(function() {
+		return magazineLuiza.catalog.getColors().then(colors => {
+			result = colors;
+		});
+	});
 
-	it('Should return all product colors', function() {
-		return magazineLuiza.catalog.getColors()
-			.then(colors => expect(colors).to.be.an('array'))
-		;
+	it('Should result to be an array of objects', function() {
+		expect(result).to.be.an('array')
+			.with.deep.property('[0]')
+			.that.is.an('object');
+	});
+
+	it('Should object result to have properties color and description',
+	function() {
+		expect(result[0]).to.have.all.keys('color', 'description');
 	});
 });
