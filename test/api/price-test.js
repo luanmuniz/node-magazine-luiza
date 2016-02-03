cfddafc51d1e20c58400cb27df79160911b5fcd3
Fernando Daciuk <f.daciuk@gmail.com>
2016-02-03 20:03:10 -0200

    Improve price api tests

--- test/api/price-test.js
+++ test/api/price-test.js
@@ -4,16 +4,36 @@ const expect = require('chai').expect;
 const getProducts = require('../helpers/get-products');
 const MagazineLuizaAPI = require('../../index');
 const PARTNER_ID = process.env.PARTNER_ID;
-const magazineLuiza = new MagazineLuizaAPI(PARTNER_ID, 'development');
+const magazineLuiza = new MagazineLuizaAPI(PARTNER_ID);
 
 describe('# [API] MAGAZINE LUIZA - CATALOG - PRICE', function() {
-	this.timeout(100000);
+	this.timeout(90000);
 
-	it('Should return product prices', function() {
+	let result;
+	before(function() {
 		const catalog = magazineLuiza.catalog;
-		return getProducts(magazineLuiza)
+		return getProducts()
 			.then(data => catalog.getPrice(data[0].id, data[0].model))
-			.then(productPrice => expect(productPrice).to.be.an('object'))
+			.then(productPrice => {
+				result = productPrice;
+			})
 		;
+	});
+
+	it('Should result of product prices to be an object', function() {
+		expect(result).to.be.an('object');
+	});
+
+	const priceProperties = [
+		'productId',
+		'productModel',
+		'valueBrl',
+		'valueCurrency',
+		'partnerId',
+		'status',
+		'message'
+	];
+	it(`Should result to have properties ${priceProperties}`, function() {
+		expect(result).to.have.all.keys(priceProperties);
 	});
 });
