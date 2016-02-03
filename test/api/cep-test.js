8abb9e6c31d88d8a42b88c0b83a81b0e39625f4c
Fernando Daciuk <f.daciuk@gmail.com>
2016-02-03 20:03:59 -0200

    Improve CEP api test

--- test/api/cep-test.js
+++ test/api/cep-test.js
@@ -3,7 +3,7 @@
 const expect = require('chai').expect;
 const MagazineLuizaAPI = require('../../index');
 const PARTNER_ID = process.env.PARTNER_ID;
-const magazineLuiza = new MagazineLuizaAPI(PARTNER_ID, 'development');
+const magazineLuiza = new MagazineLuizaAPI(PARTNER_ID);
 
 describe('# [API] MAGAZINE LUIZA - ORDER - CEP', function() {
 	this.timeout(60000);
