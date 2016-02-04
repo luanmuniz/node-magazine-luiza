6cf3d8b5021867a2da3f95c113c9db7ebf3853ab
Fernando Daciuk <f.daciuk@gmail.com>
2016-02-04 17:15:41 -0200

    Improve shipping api test

--- test/api/shipping-test.js
+++ test/api/shipping-test.js
@@ -9,9 +9,42 @@ const magazineLuiza = new MagazineLuizaAPI(PARTNER_ID, 'development');
 describe('# [API] MAGAZINE LUIZA - ORDER - CALCULATE SHIPPING', function() {
 	 this.timeout(90000);
 
-	it('Should calculate shipping', function() {
-		return calculateShipping(magazineLuiza)
-			.then(data => expect(data).to.be.an('object'))
-		;
+	 let result;
+	 before(function() {
+		return calculateShipping(magazineLuiza).then(data => {
+			result = data;
+		});
+	 });
+
+	it('Should return of calculate shipping to be an object', function() {
+		expect(result).to.be.an('object');
+	});
+
+	const shippingProperties = [
+		'sessionKey',
+		'cpf',
+		'partnerId',
+		'cep',
+		'shippingValueBrl',
+		'shippingValueCurrency',
+		'deliveryTime',
+		'deliveryDate',
+		'status',
+		'message',
+		'products'
+	];
+	it(`Should result to have properties: ${shippingProperties}`, function() {
+		expect(result).to.have.all.keys(shippingProperties);
+	});
+
+	const productsProperties = [
+		'id',
+		'model',
+		'quantity',
+		'available',
+		'stock'
+	];
+	it(`Should products to have properties: ${productsProperties}`, function() {
+		expect(result.products[0]).to.have.all.keys(productsProperties);
 	});
 });
